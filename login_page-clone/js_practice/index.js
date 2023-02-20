b51cf2ca497e656e9c43a3d0111da16f439a2672
Swetakumari082 <sweta2810kumari@gmail.com>
2023-02-20 18:34:43 +0530

    js_practice

---
+++ login_page-clone/js_practice/index.js
@@ -0,0 +1,14 @@
+console.log('it works')
+updatecolor = function(){
+    console.log("hakuna mtata")
+}
+function dosomething(){
+    alert("i am doing something")
+}
+animate_button = () =>[
+    console.log('i am animating the button')
+]
+changeMessage = (e) =>{
+    e.target.innerText='button clicked'
+    
+}
